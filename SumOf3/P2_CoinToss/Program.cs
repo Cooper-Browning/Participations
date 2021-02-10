b11caa939ee78870b7c3c16a32e0edc2bcd8e076
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-02-09 20:03:08 -0600

    Finished the code. Time to test it.

---
+++ SumOf3/P2_CoinToss/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace P2_CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string NAME = "Cooper Browning";
+
+            Console.WriteLine("Enter \"heads\" or \"tails\" >>");
+            string answer = Console.ReadLine().ToLower();
+            int userChoice;
+
+            if (answer.Equals("heads"))
+            {
+                userChoice = 1;
+            }
+            else
+            {
+                userChoice = 2;
+            }
+
+            Random rand = new Random();
+            int randomNum = rand.Next(1,3); //generate a random # between 1 and 2
+
+            if (randomNum == 1)
+            {
+                Console.WriteLine("The coin landed on heads.");
+                if (userChoice == randomNum)
+                {
+                    Console.WriteLine("\nYou guessed correctly!");
+                }
+                else
+                {
+                    Console.WriteLine("\nSorry, you guessed incorrectly :(");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nThe coin landed on tails.");
+                if (userChoice == randomNum)
+                {
+                    Console.WriteLine("\nYou guessed correctly!");
+                }
+                else
+                {
+                    Console.WriteLine("\nSorry, you guessed incorrectly :(");
+                }
+            }
+
+            Console.WriteLine($"\nDeveloper name: {NAME}");
+            
+        }
+    }
+}
