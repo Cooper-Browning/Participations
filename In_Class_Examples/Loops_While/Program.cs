c04d613045fa44e4c53713e2a974b99a31913247
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-02-23 18:01:32 -0600

    Finished while loop example

--- In_Class_Examples/Loops_While/Program.cs
+++ In_Class_Examples/Loops_While/Program.cs
@@ -6,7 +6,17 @@ namespace Loops_While
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your lucky number");
+            string answer = Console.ReadLine();
+            int luckyNbr;
+
+            while (int.TryParse(answer, out luckyNbr) == false)
+            {
+                Console.WriteLine("Sorry you entered an invalid integer. Please enter another number>>");
+                answer = Console.ReadLine();
+            }
+
+            Console.WriteLine("Yay you did it!");
         }
     }
 }
