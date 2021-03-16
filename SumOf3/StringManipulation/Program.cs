a59ae6f690efdf8d2d27c3f3ee5c40ee4ff8fd53
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-03-16 18:22:12 -0500

    Did practice with collections.

--- SumOf3/StringManipulation/Program.cs
+++ SumOf3/StringManipulation/Program.cs
@@ -7,7 +7,7 @@ namespace StringManipulation
         static string programming = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
         static void Main(string[] args)
         {
-            Console.Write(programming);
+            Console.WriteLine(programming);
             
 
             Console.WriteLine("\n\nWhat word would you like to replace in the above statement?");
@@ -18,7 +18,7 @@ namespace StringManipulation
 
             if (programming.Contains(answerReplace) == false)
             {
-                string backwards = "";
+                string backwards = string.Empty;
                 Console.WriteLine($"Sorry, I could not find your word: {answerReplace}");
                 for (int i = answerReplace.Length - 1; i >= 0; i--)
                 {
