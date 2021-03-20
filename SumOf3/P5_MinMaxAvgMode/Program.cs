ba51f2a37c92e61af43c4cc53d9778ce4f648621
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-03-20 15:53:08 -0500

    Did min max and avg

--- SumOf3/P5_MinMaxAvgMode/Program.cs
+++ SumOf3/P5_MinMaxAvgMode/Program.cs
@@ -14,7 +14,7 @@ namespace P5_MinMaxAvgMode
             do
             {
                 Console.WriteLine("Please enter an exam grade>>");
-                double answer = Convert.ToDouble(Console.ReadLine());
+                double answer = Convert.ToDouble(Console.ReadLine()) / 100;
 
                 grades.Add(answer);
 
@@ -25,14 +25,33 @@ namespace P5_MinMaxAvgMode
             } while (anotherExam.ToLower() == "y");
 
             double totalGrade = 0;
+            double min = grades[0];
+            double max = grades[0];
             for (int i = 0; i < grades.Count; i++)
             {
+                if (grades[i] < min)
+                {
+                    min = grades[i];
+                }
+                if (grades[i] > max)
+                {
+                    max = grades[i];
+                }
                 totalGrade += grades[i]; 
             }
             double avgGrade = totalGrade / grades.Count;
-            Console.WriteLine(avgGrade);
-            
-            
+            Console.WriteLine($"The average grade for your exams is: {avgGrade.ToString("P2")}");
+            Console.WriteLine($"The minimum of your grades is: {min.ToString("P2")}");
+            Console.WriteLine($"The maximum of your grades is: {max.ToString("P2")}");
+
+            //int most;
+            //double mode;
+            //for (int i = 0; i < grades.Count; i++)
+            //{
+
+           // }
+
+
             //for (int i = 0; i < grades.Count; i++)
             //{
             //    double stuGrade = grades[i];
