4e71a4a951fa16b3731a14d379df5f989a9f24bf
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-03-23 18:30:19 -0500

    Finished avg grade

--- SumOf3/All_Types_Of_Collection/Program.cs
+++ SumOf3/All_Types_Of_Collection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace All_Types_Of_Collection
 {
@@ -6,6 +7,7 @@ namespace All_Types_Of_Collection
     {
         static void Main(string[] args)
         {
+            //Number 1
             string[] subject = new string[3];
             string[] number = new string[3];
             for (int i = 0; i < subject.Length; i++)
@@ -22,6 +24,30 @@ namespace All_Types_Of_Collection
                 string course = subject[i] + number[i];
                 Console.WriteLine($"{course}");
             }
+
+            //Number 2
+            List<double> grades = new List<double>();
+
+            string answer;
+            do
+            {
+                Console.WriteLine("Please enter a grade:");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                grades.Add(grade);
+
+                Console.WriteLine("Do you have another grade to enter? Yes or no:");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+
+            double totalGrade = 0;
+            foreach (double grade in grades)
+            {
+                totalGrade += grade;
+            }
+            double average = totalGrade / grades.Count;
+            Console.WriteLine($"You average grade is: {average.ToString("N2")}");
+
         }
     }
 }
