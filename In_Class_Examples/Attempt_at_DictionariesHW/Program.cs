87b323ba0d7a563370cadb5a9949ca652c0739a6
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-04-05 18:49:13 -0500

    HW5 problem 3

---
+++ In_Class_Examples/Attempt_at_DictionariesHW/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Attempt_at_DictionariesHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] lines = File.ReadAllLines("Jane Eyre.txt");
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            
+            string[] pieces = new string[lines.Length-1];
+           for (int i = 0; i < lines.Length; i++)
+           {
+               pieces = lines[i].Split(' ');
+               for (int j = 0; j < pieces.Length; j++)
+               {
+                   pieces[j] = pieces[j].Replace(",", "").Replace("-", "").Replace("_", "").Replace("(", "").Replace(")", "")
+                       .Replace("?", "").Replace(",", "*").Replace("/", "").Replace(":", "").Replace(".", "");
+                   if (words.ContainsKey(pieces[j]) == false)
+                   {
+                       words.Add(pieces[j], 1);
+                   }
+                   else
+                   {
+                       words[pieces[j]] += 1;
+                   }
+               }   
+
+           }
+
+           foreach (string key in words.Keys)
+           {
+               Console.WriteLine($"{key}\t{words[key]}");
+           }
+
+
+           for (int i = 0; i < pieces.Length; i++)
+           {
+               if (words.ContainsKey(pieces[i]) == false)
+               {
+                   words.Add(pieces[i], 1);
+               }
+               else
+               {
+                   words[pieces[i]] += 1;
+               }
+           }
+            
+            foreach (string piece in pieces)
+            {
+                Console.WriteLine(piece);
+            }
+
+            /*
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            */
+        }
+    }
+}
