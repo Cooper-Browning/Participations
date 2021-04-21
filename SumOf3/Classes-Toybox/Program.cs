6f83e2fdab4e7413afa2f09641de4aa3a1bb1aa6
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-04-20 23:26:04 -0500

    Worked on Toybox

--- SumOf3/Classes-Toybox/Program.cs
+++ SumOf3/Classes-Toybox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Classes_Toybox
 {
@@ -6,6 +7,65 @@ namespace Classes_Toybox
     {
         static void Main(string[] args)
         {
+            List<ToyBox> AllToyBoxes = new List<ToyBox>();
+            string moreToyBoxes;
+            int toyIndex = 0;
+            do
+            {
+                
+                ToyBox BoxAdding = new ToyBox();
+                Console.WriteLine("Who is the owner of this toybox?");
+                BoxAdding.Owner = Console.ReadLine();
+                Console.WriteLine("Where is this toybox kept?");
+                BoxAdding.Location = Console.ReadLine();
+                AllToyBoxes.Add(BoxAdding);
+
+                string moreToys;
+                do
+                {
+                    Console.WriteLine("What is the name of your toy you want to add to this toybox?");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Who is the manufacturer of the toy?");
+                    string man = Console.ReadLine();
+                    Console.WriteLine("What was the price of your toy?");
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("What notes do you want to add for the toy?");
+                    string notes = Console.ReadLine();
+                    AllToyBoxes[toyIndex].Toys.Add(new Toy(man, name, price, notes));
+
+                    Console.WriteLine("Would you like to enter another toy into your toybox?");
+                    moreToys = Console.ReadLine();
+
+
+                } while (moreToys.ToLower()[0] == 'y');
+
+                Console.WriteLine("Do you have another toybox with toys that you want to add?");
+                moreToyBoxes = Console.ReadLine();
+
+                toyIndex++;
+
+            } while (moreToyBoxes.ToLower()[0] == 'y');
+
+            for (int i = 0; i < AllToyBoxes.Count; i++)
+            {
+                Console.WriteLine($"\nContents of ToyBox {i + 1}:\n");
+                for (int j = 0; j < AllToyBoxes[i].Toys.Count; j++)
+                {
+                    Console.WriteLine(AllToyBoxes[i].Toys[j].ToString());
+                }
+            }
+
+            for (int i = 0; i < AllToyBoxes.Count; i++)
+            {
+                Console.WriteLine($"\nRandom toy in toybox {i + 1}:\n");
+                Console.WriteLine(AllToyBoxes[i].GetRandomToy().ToString());
+            }
+            /*
+            foreach (ToyBox tb in AllToyBoxes)
+            {
+                Console.WriteLine(tb.GetRandomToy().ToString());
+            }
+            
             ToyBox myToys = new ToyBox();
             Console.WriteLine("What is your name?");
             myToys.Owner = Console.ReadLine();
@@ -31,9 +91,9 @@ namespace Classes_Toybox
 
             } while (moreToys.ToLower()[0] == 'y');
 
+            */
 
-
-            Console.WriteLine(myToys.GetRandomToy().ToString());
+            //Console.WriteLine(myToys.GetRandomToy().ToString());
         }
     }
 }
