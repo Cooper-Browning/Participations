c876154b30ac885640cbacca1e4e93e20fa8096c
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-04-22 17:25:13 -0500

    Finished toy box participation

--- SumOf3/Classes-Toybox/Program.cs
+++ SumOf3/Classes-Toybox/Program.cs
@@ -60,40 +60,7 @@ namespace Classes_Toybox
                 Console.WriteLine($"\nRandom toy in toybox {i + 1}:\n");
                 Console.WriteLine(AllToyBoxes[i].GetRandomToy().ToString());
             }
-            /*
-            foreach (ToyBox tb in AllToyBoxes)
-            {
-                Console.WriteLine(tb.GetRandomToy().ToString());
-            }
-            
-            ToyBox myToys = new ToyBox();
-            Console.WriteLine("What is your name?");
-            myToys.Owner = Console.ReadLine();
-            Console.WriteLine("Where do you keep your toybox?");
-            myToys.Location = Console.ReadLine();
 
-            string moreToys;
-            do
-            {
-                Console.WriteLine("Who is the manufacturer of the toy?");
-                string man = Console.ReadLine();
-                Console.WriteLine("What is the name of your toy?");
-                string name = Console.ReadLine();
-                Console.WriteLine("What was the price of your toy?");
-                double price = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("What notes do you want to add for the toy?");
-                string notes = Console.ReadLine();
-                myToys.Toys.Add(new Toy(man, name, price, notes));
-
-                Console.WriteLine("Would you like to enter another toy into your toybox?");
-                moreToys = Console.ReadLine();
-
-
-            } while (moreToys.ToLower()[0] == 'y');
-
-            */
-
-            //Console.WriteLine(myToys.GetRandomToy().ToString());
         }
     }
 }
