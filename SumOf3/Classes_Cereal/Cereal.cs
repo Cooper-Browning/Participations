08309b83c8f46b222fae9706613ab4fd51ce123a
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-04-27 18:21:39 -0500

    Finished cereal participation

---
+++ SumOf3/Classes_Cereal/Cereal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Cereal
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+        public Cereal()
+        {
+            Manufacturer = String.Empty;
+            Name = String.Empty;
+            Calories = 0;
+            Cups = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Manufacturer: {Manufacturer} Calories: {Calories} Cups: {Cups}";
+        }
+    }
+}
