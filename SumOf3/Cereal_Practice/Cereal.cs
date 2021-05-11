f2a0bfc6795d41435c2a1ff56dece27094042167
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-05-11 11:59:18 -0500

    Practiced cereals class

---
+++ SumOf3/Cereal_Practice/Cereal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cereal_Practice
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+
+
+        public Cereal()
+        {
+            Manufacturer = String.Empty;
+            Name = String.Empty;
+            Calories = 0;
+            Cups = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Manufacturer: {Manufacturer} Calories: {Calories} Cups: {Cups}";
+        }
+    }
+}
