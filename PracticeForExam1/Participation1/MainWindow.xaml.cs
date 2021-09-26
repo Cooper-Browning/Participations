a3b8a155c950dc922984b260380dbc9a4d32aa95
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-09-26 14:00:21 -0500

    Working on practice problems

---
+++ PracticeForExam1/Participation1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Participation1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int YEAR = 2021;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lblAge.Content = "";
+            lblDogAge.Content = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int bday;
+            int dogAge;
+
+            bday = YEAR - Convert.ToInt32(txtAge.Text);
+            dogAge = Convert.ToInt32(txtDogAge.Text) * 7;
+
+            lblAge.Content = $"You were born in {bday}";
+            lblDogAge.Content = $"Your dog is {dogAge} years old in dog years";
+
+
+        }
+    }
+}
