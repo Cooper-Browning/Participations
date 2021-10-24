789b34a9f74beab306c0e1f9d6cfe81b669307c5
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-10-24 16:41:53 -0500

    Working on practice

---
+++ PrepForExam2/SerializationPractice/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerializationPractice
+{
+    public class Game
+    {
+        public string Name {get; set;}
+        public string Platform {get; set;}
+        public string ReleaseDate {get; set;}
+        public string Summary {get; set;}
+        public int MetaScore { get; set; }
+        public string UserReview { get; set; }
+
+        public Game()
+        {
+            Name = string.Empty;
+            Platform = string.Empty;
+            ReleaseDate = string.Empty;
+            Summary = string.Empty;
+            MetaScore = 0;
+            UserReview = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ PrepForExam2/SerializationPractice/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerializationPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Game> games = new List<Game>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            cboGame.Items.Add("All");
+            var allGames = File.ReadAllLines("all_games (2).csv").Skip(1);
+
+            foreach (var vidGame in allGames)
+            {
+                //name,platform,release_date,summary,meta_score,user_review
+                string[] currentGame = vidGame.Split(',');
+                Game g = new Game();
+                g.Name = currentGame[0];
+                g.Platform = currentGame[1];
+                g.ReleaseDate = currentGame[2];
+                g.Summary = currentGame[3];
+                g.MetaScore = Convert.ToInt32(currentGame[4]);
+                g.UserReview = currentGame[5];
+
+                cboGame.Items.Add("All");
+                games.Add(g);
+                if (!cboGame.Items.Contains(g.Platform))
+                {
+                    cboGame.Items.Add(g.Platform);
+                }
+
+                lstGames.Items.Add(g);
+
+            }
+
+        }
+
+        private void cboGame_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lstGames.Items.Clear();
+            if (cboGame.SelectedItem == "All")
+            {
+                foreach (var game in games)
+                {
+                    lstGames.Items.Add(game);
+                }
+            }
+            else
+            {
+                foreach (var game in games)
+                {
+                    if (game.Platform == cboGame.SelectedItem)
+                    {
+                        lstGames.Items.Add(game);
+                    }
+                }
+            }
+        }
+    }
+}
