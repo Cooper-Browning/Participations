ec55764456c7b3589c8aff5ace1a4dec217cc228
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-10-26 20:48:29 -0500

    Exam practice working hw4

---
+++ PrepForExam2Pt2/HW6Prob2Prac/WineInfo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW6Prob2Prac
+{
+    /// <summary>
+    /// Interaction logic for WineInfo.xaml
+    /// </summary>
+    public partial class WineInfo : Window
+    {
+        public WineInfo()
+        {
+            InitializeComponent();
+        }
+
+        public void PopulateData(WineAPI w)
+        {
+            txtCountry.Text = w.country;
+            txtPrice.Text = w.price;
+            lblTitle.Content = w.title;
+
+        }
+    }
+}
---
+++ PrepForExam2Pt2/Hw4VidPrac/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hw4VidPrac
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            medRandVid.LoadedBehavior = MediaState.Manual;
+            btnPlayPause.IsEnabled = false;
+        }
+
+        private void btnGetVid_Click(object sender, RoutedEventArgs e)
+        {
+            string webUrl = "http://pcbstuou.w27.wh-2.com/webservices/3033/api/random/video";
+            RandomVideoAPI vid;
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(webUrl).Result;
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    vid = JsonConvert.DeserializeObject<RandomVideoAPI>(json);
+                    medRandVid.Source = new Uri(vid.url);
+                    medRandVid.Pause();
+                    btnPlayPause.Content = "Play";
+                    btnPlayPause.IsEnabled = true;
+                }
+            }
+
+
+        }
+
+        private void btnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+            string status = btnPlayPause.Content.ToString().ToLower();
+            switch (status)
+            {
+                case "play":
+                    medRandVid.Play();
+                    btnPlayPause.Content = "Pause";
+                    break;
+                case "pause":
+                    medRandVid.Pause();
+                    btnPlayPause.Content = "Play";
+                    break;
+            }
+            /*
+            if (btnGetVid.Content == "Play")
+            {
+                medRandVid.Play();
+                btnGetVid.Content = "Pause";
+            }
+            else if (btnGetVid.Content == "Pause")
+            {
+                medRandVid.Pause();
+                btnGetVid.Content = "Play";
+            }
+            */
+        }
+    }
+}
