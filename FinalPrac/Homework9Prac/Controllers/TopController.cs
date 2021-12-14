7525752efbf98eb447c5879ef322652d3c41e8f4
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-12-14 15:42:14 -0600

    Finished practicing hw 9

---
+++ FinalPrac/Homework9Prac/Controllers/TopController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework9Prac.Models;
+
+namespace Homework9Prac.Controllers
+{
+    public class TopController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: Top
+        public ActionResult Movies(int? number)
+        {
+           
+            if (number == null)
+            {
+                var movies = db.Movies.Include(m => m.Director).OrderByDescending(x => x.gross).Take(10);
+                return View(movies.ToList());
+            }
+            else
+            {
+                var movies = db.Movies.Include(m => m.Director).OrderByDescending(x => x.gross).Take((int)number);
+                return View(movies.ToList());
+            }
+            
+            
+        }
+
+        // GET: Top/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ FinalPrac/Homework9Prac/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homework9Prac.MvcApplication" Language="C#" %>
